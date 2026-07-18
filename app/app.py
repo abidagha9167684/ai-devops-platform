1306472c59f33d4234fb9e5e83442f52eec94d8f
Abidullah Agha <abidagha172@gmail.com>
2026-07-19 00:22:55 +0200

    Add Prometheus application metrics

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 
@@ -18,4 +19,5 @@ def health():
 
 
 if __name__ == "__main__":
+    metrics = PrometheusMetrics(app)
     app.run(host="0.0.0.0", port=5000)
